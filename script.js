71b4276ee9e7767d126ec3ab2fd524ba9044eb44
KsyKsy-dev <ksy.codedev@gmail.com>
2023-07-22 11:47:43 +0200

    add function

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-console.log("go");
-function a() {
-  return a;
+"use strict";
+
+// expresion
+let ask= function(q,y,n){
+    if((confirm(q)) y();
+    else n();
 }
+ask('agree?', function(){console.log("y");}, function(){console.log(n);})
