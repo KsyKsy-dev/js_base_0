9a5e8b6189f1aef7ef8b18ed007d860fd0b9666a
KsyKsy-dev <ksy.codedev@gmail.com>
2023-07-24 11:55:03 +0200

    this

--- script.js
+++ script.js
@@ -2,9 +2,27 @@
 
 //Object's method pre declared
 
-let user = {};
-function sey() {
-  console.log("Say hi!");
+// let user = {};
+// function sey() {
+//   console.log("Say hi!");
+// }
+// user.sey = sey;
+// user.sey();
+
+// let user = {
+//   name: "2",
+//   say() {
+//     console.log("name");
+//   },
+// };
+// user.say();
+
+//this function
+let arr = { name: "arr" };
+let arr1 = { name: "arr1" };
+
+function testThis(name) {
+  console.log(name);
 }
-user.sey = sey;
-user.sey();
+arr.f = testThis;
+arr.f(this.name);
