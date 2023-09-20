aaad6f1f093290d2550d4918c66bf9954483ab26
KsyKsy-dev <ksy.codedev@gmail.com>
2023-09-20 19:21:16 +0200

    add indexOf

--- script.js
+++ script.js
@@ -403,7 +403,39 @@
 
 // srting are amutuble
 
-let m = "TWO";
+// let m = "TWO";
 
-let n = m[0] + m[2];
-console.log(n);
+// let n = m[0] + m[2];
+// console.log(n);
+
+// indexOf()
+// let str = "As sly as a fox, as strong as an ox";
+
+// let target = "as"; // let's look for it
+// console.log(str.length);
+// let pos = 0;
+// while (true) {
+//   let foundPos = str.indexOf(target, pos);
+//   console.log(foundPos);
+//   if (foundPos == -1) break;
+
+//   console.log(`Found at ${foundPos}`);
+//   pos = foundPos + 1; // continue the search from the next position
+// }
+
+// let str = "As sly as a fox, as strong as an ox";
+// let target = "as";
+
+// let pos = -1;
+// while ((pos = str.indexOf(target, pos + 1)) != -1) {
+//   console.log(pos);
+// }
+
+// indexOf() star value is 0
+
+let fr = "some dummy text";
+if (fr.indexOf("some") != -1) {
+  console.log("go");
+} else {
+  console.log("arrived");
+}
